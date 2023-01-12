d3e6afc4b14c8748c5845b8727aa08eafa3580f1
Larson <jordan.laude.9@gmail.com>
2023-01-12 14:01:18 +0100

    Ajout des scripts de creation et injection de donnees pour la BDD

--- database/creation.sql
+++ database/creation.sql
@@ -1,6 +1,4 @@
-﻿drop table if exists Club;
-drop table if exists Utilisateur;
-drop table if exists Concours;
+﻿drop table if exists Concours;
 drop table if exists President;
 drop table if exists Administrateur;
 drop table if exists Directeur;
@@ -8,79 +6,73 @@ drop table if exists Evaluateur;
 drop table if exists Evaluation;
 drop table if exists Dessin;
 drop table if exists Competiteur;
+drop table if exists Utilisateur;
 
+drop table if exists Club;
 Create Table Club
 (    
-idClub    	     smallint NOT NULL auto_increment,
+idClub    	     smallint NOT NULL AUTO_INCREMENT,
 nom  		     Varchar (40) NOT NULL, 
 adresse	         Varchar (120) NOT NULL,
-numTelephone     Integer NOT NULL,
-nombreAdherents  Integer NOT NULL,
+numTelephone     char(10) NOT NULL,
 ville			 Varchar (50) NOT NULL,
-departement		 Integer NOT NULL,
+departement		 Varchar (60) NOT NULL,
 region			 Varchar (80) NOT NULL,
 PRIMARY KEY (idClub)
 ) ;
 
 Create Table Utilisateur
 (    
-idUtilisateur   Varchar (40) NOT NULL,
+idUtilisateur   smallint NOT NULL AUTO_INCREMENT,
 nom		         Varchar (40) NOT NULL, 
 prenom	         Varchar (40) NOT NULL,
 adresse     	 Varchar (120) NOT NULL,
 login  			 Varchar (40) NOT NULL,
 motDePasse       Varchar (40) NOT NULL,
+dateInscription dateTime NOT NULL,
 
-idClub    	     smallint NOT NULL auto_increment,
+idClub    	     smallint NOT NULL,
 
-PRIMARY KEY (idUtilisateur)
+PRIMARY KEY (idUtilisateur),
 FOREIGN KEY (idClub) REFERENCES Club (idClub)
-) ;
-
-Create Table Concours
-(    
-idConcours  	 Integer NOT NULL auto_increment,
-theme		     Varchar (40) NOT NULL, 
-dateDebut	     dateTime NOT NULL,
-dateFin     	 dateTime NOT NULL,
-etat  			 enum('pas commencé', 'en cours', 'attente', 'résultat', 'évalué'),
-
-prime 			 Varchar (50) NOT NULL,
-
-PRIMARY KEY (idConcours)
-FOREIGN KEY (prime) REFERENCES President (prime)
-) ;
+);
 
 Create Table President
-(    
-prime       	Varchar (50) NOT NULL,
-) INHERITS (Utilisateur);
+(
+idUtilisateur   smallint NOT NULL,
+FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur (idUtilisateur)   
+);
 
 Create Table Administrateur
 (
-dateDebut dateTime NOT NULL,
-) INHERITS (Utilisateur);
+idUtilisateur   smallint NOT NULL,
+FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur (idUtilisateur)   
+);
 
 Create Table Directeur
 (
-dateDebut dateTime NOT NULL,
-) INHERITS (Utilisateur);
+idUtilisateur   smallint NOT NULL,
+FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur (idUtilisateur)   
+
+);
 
 Create Table Evaluateur
 (
+idUtilisateur   smallint NOT NULL,
 specialite  Varchar (40) NOT NULL,
-) INHERITS (Utilisateur);
+FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur (idUtilisateur)   
+);
 
-Create Table Evaluation
+Create Table Competiteur
 (    
-dateEvaluation   dateTime NOT NULL,
-note		     Integer NOT NULL, 
-commentaire	     Varchar (250),
-) ;
+idUtilisateur   smallint NOT NULL,
+datePremiereParticipation  dateTime NOT NULL,
+FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur (idUtilisateur)   
+);
 
 Create Table Dessin
 (    
-idDessin  		 Integer NOT NULL,
+idDessin  		 smallint NOT NULL,
 commentaire      Varchar (250), 
 classement	     Integer NOT NULL,
 dateRemise     	 dateTime NOT NULL,
@@ -88,10 +80,27 @@ leDessin  		 char NOT NULL,
 PRIMARY KEY (idDessin)
 ) ;
 
-
-
-Create Table Competiteur
+Create Table Evaluation
 (    
-datePremiereParticipation  dateTime NOT NULL,
-) INHERITS (Utilisateur);
+dateEvaluation   dateTime NOT NULL,
+note		     Integer NOT NULL, 
+commentaire	     Varchar (250),
+idDessin  		 smallint NOT NULL,
+FOREIGN KEY (idDessin) REFERENCES Dessin (idDessin)   
+) ;
 
+Create Table Concours
+(    
+idConcours  	 smallint NOT NULL AUTO_INCREMENT,
+theme		     Varchar (40) NOT NULL, 
+dateDebut	     dateTime NOT NULL,
+dateFin     	 dateTime NOT NULL,
+etat  			 enum('pas commencé', 'en cours', 'attente', 'résultat', 'évalué'),
+prime 			 Varchar (50) NOT NULL,
+
+idPresident   smallint NOT NULL,
+
+PRIMARY KEY (idConcours),
+FOREIGN KEY (idPresident) REFERENCES Utilisateur (idUtilisateur)   
+
+);
---
+++ database/insert_club.sql
@@ -0,0 +1,35 @@
+﻿INSERT INTO Club(nom, adresse, numTelephone, ville, departement, region)
+VALUES
+
+('nom', '1 rue des fleurs', '0601010101', 'Concarneau', 'Finistère', 'Bretagne'),
+('nom', '2 rue des bouchers', '0601010102', 'Quimper', 'Finistère', 'Bretagne'),
+
+('nom', '3 rue des oliviers', '0601010103', 'Vannes', 'Morbihan', 'Bretagne'),
+('nom', '4 rue de la trinquette', '0601010104', 'Vannes', 'Morbihan', 'Bretagne'),
+
+('nom', '5 rue des pommiers', '0601010105', 'Givet', 'Ardennes', 'Grand Est'),
+('nom', '6 rue des poiriers', '0601010106', 'Sedan', 'Ardennes', 'Grand Est'),
+
+('nom', '7 rue des stantors', '0601010107', 'Verdun', 'Meuse', 'Grand Est'),
+('nom', '8 rue des albators', '0601010108', 'Étain', 'Meuse', 'Grand Est'),
+
+('nom', '9 rue des zinzins', '0601010109', 'Les Herbiers', 'Vendée', 'Pays de la Loire'),
+('nom', '10 rue des philipes', '0601010110', 'Les Landes Genusson', 'Vendée', 'Pays de la Loire'),
+
+('nom', '11 rue des oiseaux', '0601010111', 'Le Mans', 'Sarthe', 'Pays de la Loire'),
+('nom', '12 rue des potiers', '0601010112', 'Le Mans', 'Sarthe', 'Pays de la Loire'),
+
+('nom', '13 rue de lane', '0601010113', 'Toulouse', 'Haute-Garonne', 'Occitanie'),
+('nom', '14 rue des pédéstriers', '0601010114', 'Muret', 'Haute-Garonne', 'Occitanie'),
+
+('nom', '15 rue des brababam', '0601010115', 'Viols-Le-Fort', 'Hérault', 'Occitanie'),
+('nom', '16 rue des broubouboum', '0601010116', 'Viols-Le-Fort', 'Hérault', 'Occitanie');
+
+
+
+
+
+
+
+
+
