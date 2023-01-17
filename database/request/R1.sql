d34e9626233d80492205969619ad43370030f368
Diabltica <Diabltica@gmail.com>
2023-01-17 16:05:57 +0100

    complete first request

--- database/request/R1.sql
+++ database/request/R1.sql
@@ -1,1 +1,43 @@
-fsfsf
+SELECT
+    U.nom,
+    U.dateNaissance,
+    U.adresse,
+    Con.dateDebut,
+    Con.dateFin,
+    Con.descriptionConcours,
+    Clu.departement,
+    Clu.region
+FROM
+    Utilisateur U,
+    Concours Con,
+    Club Clu
+WHERE
+    (
+    SELECT
+        C.idCompetiteur
+    FROM
+        Competiteur C
+    WHERE
+        U.idUtilisateur = C.idCompetiteur AND(
+        SELECT
+            D.idCompetiteur
+        FROM
+            Dessin D
+        WHERE
+            C.idCompetiteur = D.idCompetiteur AND(
+            SELECT
+                Conc.idConcours
+            FROM
+                Concours Conc
+            WHERE
+                D.idConcours = Conc.idConcours AND YEAR(Conc.dateFin) = 2021
+        )
+    ) AND(
+    SELECT
+        Club.idClub
+    FROM
+        Club
+    WHERE
+        U.idClub = Club.idClub
+)
+);
