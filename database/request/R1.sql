16e9dad75f6e9e15c8dd6a7889989802bf4d17bd
Diabltica <Diabltica@gmail.com>
2023-01-18 11:31:26 +0100

    correct request

--- database/request/R1.sql
+++ database/request/R1.sql
@@ -1,43 +1,25 @@
-SELECT
+SELECT DISTINCT
     U.nom,
     DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),U.dateNaissance)), '%Y') + 0 AS age,
     U.adresse,
+    Con.idConcours,
     Con.dateDebut,
     Con.dateFin,
     Con.descriptionConcours,
+    Clu.idClub,
     Clu.departement,
     Clu.region
 FROM
     Utilisateur U,
     Concours Con,
-    Club Clu
+    Club Clu,
+    Competiteur C,
+    Dessin D
 WHERE
-    (
-    SELECT
-        C.idCompetiteur
-    FROM
-        Competiteur C
-    WHERE
-        U.idUtilisateur = C.idCompetiteur AND(
-        SELECT
-            D.idCompetiteur
-        FROM
-            Dessin D
-        WHERE
-            C.idCompetiteur = D.idCompetiteur AND(
-            SELECT
-                Conc.idConcours
-            FROM
-                Concours Conc
-            WHERE
-                D.idConcours = Conc.idConcours AND YEAR(Conc.dateDebut) = 2021
-        )
-    ) AND(
-    SELECT
-        Club.idClub
-    FROM
-        Club
-    WHERE
-        U.idClub = Club.idClub
-)
-);
+	D.idConcours = Con.idConcours
+    AND
+    D.idCompetiteur = C.idCompetiteur
+	AND
+    U.idClub = Clu.idClub
+    AND 
+    U.idUtilisateur = C.idCompetiteur;
